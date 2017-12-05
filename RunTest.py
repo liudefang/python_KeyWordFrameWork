3a39cd1a1f7134d376a0d898e07404a1b3310e6f
liudefang <15989576517@139.com>
2017-12-06 00:00:39 +0800

    python关键字驱动

---
+++ RunTest.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+# @Time    : 2017-11-27 23:11
+# @Author  : mike.liu
+# @File    : RunTest.py
+from testScripts.TestSendMailWithAttachment import TestSendMailWithAttachment
+
+if __name__ == '__main__':
+    TestSendMailWithAttachment()
